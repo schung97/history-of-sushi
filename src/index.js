b5331204e532ddbdd65fdc196e12862050814323
Silvia Chung <schung97@gmail.com>
2018-03-16 16:27:34 -0400

    css foundation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './STORE';
-import './stylesheet.css';
+import './_css/master.css';
 
 ReactDOM.render(
     <Provider store={store}>
